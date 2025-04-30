693c7e903b1002a14c042ee61e17b334a78c0c6f
brampedgex <70824773+brampedgex@users.noreply.github.com>
2025-04-29 23:55:12 -0700

    Add CPM and spdlog

--- dll/src/Main.cpp
+++ dll/src/Main.cpp
@@ -4,10 +4,17 @@
 
 #include <thread>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/msvc_sink.h>
+
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, [[maybe_unused]] LPVOID lpvReserved) {
     if (fdwReason == DLL_PROCESS_ATTACH) {
         std::thread([=]() {
-            OutputDebugStringA("Hello from Aether!");
+            spdlog::default_logger()->sinks().assign( {
+                std::make_shared<spdlog::sinks::msvc_sink_mt>()
+            });
+
+            spdlog::info("Hello from Aether!");
 
             // With std::thread, FreeLibrary should be used instead of FreeLibraryAndExitThread
             FreeLibrary(hinstDLL);
